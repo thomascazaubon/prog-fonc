5dbf5ed0ca17f7fb5ee5ecbf6f2ae7aa8855a4c4
JackyCrew <thomascazaubon@outlooK.fr>
2018-11-06 15:13:15 +0100

    Fin graph.map

--- lib/graph.ml
+++ lib/graph.ml
@@ -41,13 +41,21 @@ let v_iter gr f = List.iter (fun (id, out) -> f id out) gr
 
 let v_fold gr f acu = List.fold_left (fun acu (id, out) -> f acu id out) acu gr
 
-let map f gr =
+(* applique f sur une liste d'arcs *)
+let umap f arcs =
+  let rec iter f acu = function
+    |[] -> List.rev acu
+    |(a,b)::tail -> iter f ((a,f b)::acu) tail
+  in
+  iter f [] arcs
+
+let map gr f =
   let rec iter f acu = function
     |[] -> List.rev acu
     |node::tail -> match node with
+      (* Le noeud est un puits *)
       |(a,[]) -> iter f ((a,[])::acu) tail
-      |(a,l) -> match l with
-        |(a,b) -> 
-        |(a,b)::tail ->
+      (* Le noeud contient des arcs sortants *)
+      |(a,l) -> iter f ((a, umap f l)::acu) tail
   in
   iter f [] gr
