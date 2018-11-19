1eeb52d1e4d0d206fe88fd49c0a46c0506b5f96e
JackyCrew <thomascazaubon@outlooK.fr>
2018-11-19 13:15:23 +0100

    make_flow et convert_flow faits !

--- app/algo.ml
+++ app/algo.ml
@@ -1,2 +1,5 @@
 let make_flow gr =
-  Graph.map gr (fun (a,b) -> (a, 0, b))
+  Graph.map gr (fun a -> (0, int_of_string a))
+
+let convert_flow gr =
+  Graph.map gr (fun (a,b) -> (string_of_int a)^"/"^(string_of_int b))
