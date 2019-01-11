2fa6967b8b4fec9a8edbc9d412265241e9a0bf97
M. Gaillard Paul <pgaillar@insa-10222>
2019-01-11 15:58:33 +0100

    just somes comment

--- app/multisource.ml
+++ app/multisource.ml
@@ -1,13 +1,7 @@
 open Graph
 open Algo
-(* 2 -> 1 -> 3 -> 5 ("2", [("1", (4,4)); ("3", (14,14)); ("5",(20,20))]) *)
+(* Used to generate a max flow graph with multi source and multi sink *)
 let () =
-
- (* if Array.length Sys.argv <> 5 then
-    begin
-      Printf.printf "\nUsage: %s infile source sink outfile\n\n%!" Sys.argv.(0) ;
-      exit 0
-    end ;*)
 
   let infile = Sys.argv.(1)
   and outfile = Sys.argv.(4)
@@ -17,6 +11,7 @@ let () =
   and nbsink = int_of_string Sys.argv.(3)
   and lsource = []
   in
+  (* Retrieve the sources and sink given in argument in order to return a tuple (list of sources, list of sink)  *)
   let multi nbsource nbsink lsource =
 	let rec multi_rec nbsource nbsink lsource lsink i = 
 		if (nbsource<1) then 
@@ -26,21 +21,13 @@ let () =
 	in multi_rec nbsource nbsink lsource [] 5
   in let lsourcedest = multi nbsource nbsink lsource in
 
-  (*let testlis lsourcedest =
-	let rec test lsourcedest = match lsourcedest with
-		|([],dest) -> (match dest with
-			|[] -> Printf.printf "FINI\n%!"
-			|i :: rest -> let () = Printf.printf "Dest : %s\n%!" i in test ([],rest))
-		|(i :: rest,dest) -> let () = Printf.printf "Source : %s\n%!" i in test (rest,dest)
-	in test lsourcedest
-  in let () = testlis lsourcedest in*)
-
   (* Open file *)
   let graph = Gfile.from_file infile in
+  (* Add big source and big sink *)
   let graph = Algo.make_multi graph lsourcedest in
   (* export .dot *)
   let graph = Algo.make_flow graph in
-(*  let graph = Algo.make_residual graph in *)
+
   let graph = Algo.ford_fulkerson graph "source" "dest" in
   let graph = Algo.convert_flow graph in
   let () = Gfile.export "FF_export" graph in
